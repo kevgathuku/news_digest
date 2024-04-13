9b30ca014ef35fd4bfd25ead547b532cfcf4dea2
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-04-13 18:39:47 +0200

    Mail config

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_mail import Mail, Message
 import flask_admin as admin
 from flask_admin.contrib.peewee import ModelView
 from huey import RedisHuey, crontab
@@ -8,14 +9,27 @@ from models import SavedLink, SearchTerm
 from searcher import FeedSearcher, RedditSearcher
 
 app = Flask(__name__)
+
+# config
 app.config["SECRET_KEY"] = (
     "\xf5\xa7\xec\xd0I\xe3\x9aLMB\xd6\xa9F\x8e\x03\x8a]9\xb4\xc8\x1cm\x08\xfc"
 )
-app.config["REDIS_URL"] = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
+app.config["REDIS_URL"] = os.environ.get("REDISCLOUD_URL", "redis://localhost:6379/0")
+app.config.update(
+    MAIL_SERVER="smtp.googlemail.com",
+    MAIL_PORT=465,
+    MAIL_USE_TLS=False,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=os.environ.get("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD"),
+    MAIL_DEFAULT_SENDER=os.environ.get("MAIL_USERNAME"),
+    MAIL_DEFAULT_RECIPIENT=os.environ.get("MAIL_RECIPIENT"),
+)
 
-huey = RedisHuey("news-digest", url=app.config["REDIS_URL"])
-
+# extensions
 admin = admin.Admin(app, name="News Digest")
+huey = RedisHuey("news-digest", url=app.config["REDIS_URL"])
+mail = Mail(app)
 
 SEARCHERS = [
     FeedSearcher("https://news.ycombinator.com/rss"),
@@ -46,6 +60,16 @@ def add_numbers(a, b):
     return a + b
 
 
+def send_email(subject, text_body):
+    with app.app_context():
+        msg = Message(
+            subject,
+            recipients=app.config["MAIL_DEFAULT_RECIPIENT"],
+        )
+        msg.body = text_body
+        mail.send(msg)
+
+
 @huey.periodic_task(crontab(minute="0", hour="7"))
 def content_search():
     logger = app.logger
@@ -65,8 +89,9 @@ def content_search():
                     links.append(result)
 
     if links:
+        logger.info("Found %s links", len(links))
         digest = "\n".join('"%s"  ->  %s' % (link.title, link.url) for link in links)
-        # send_email(my_email_address, "Article digest", digest)
+        send_email("Your Latest Programming News digest", digest)
 
 
 if __name__ == "__main__":
