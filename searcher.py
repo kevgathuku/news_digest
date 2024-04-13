fd67e44954e59b08eb950ca52b64ad04895dd071
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-04-13 17:20:17 +0200

    Add reddit and RSS feed searcher

---
+++ searcher.py
@@ -0,0 +1,47 @@
+import json
+from xml.etree import ElementTree as etree
+from urllib.request import Request, urlopen
+from collections import namedtuple
+
+Link = namedtuple("Link", ("title", "url"))
+
+
+class Searcher(object):
+    user_agent = "PythonicHacks/0.1"
+
+    def __init__(self, url):
+        self.url = url
+
+    def fetch(self):
+        request = Request(self.url)
+        request.add_header("User-Agent", self.user_agent)
+        return urlopen(request).read()
+
+    def search(self, search_terms):
+        content = self.fetch()
+        for link in self.extract_links(content):
+            for search_term in search_terms:
+                if search_term.test(link.title.lower()):
+                    yield link
+                    break
+
+    def extract_links(self, content):
+        raise NotImplementedError
+
+
+class FeedSearcher(Searcher):
+    def extract_links(self, content):
+        tree = etree.fromstring(content)
+        articles = tree.findall("channel/item")
+        for article in articles:
+            yield Link(title=article.findtext("title"), url=article.findtext("link"))
+
+
+class RedditSearcher(Searcher):
+    def extract_links(self, content):
+        content = json.loads(content)
+        links = content["data"]["children"]
+        for link in links:
+            link_data = link["data"]
+            if link_data["selftext"] == "":
+                yield Link(title=link_data["title"], url=link_data["url"])
